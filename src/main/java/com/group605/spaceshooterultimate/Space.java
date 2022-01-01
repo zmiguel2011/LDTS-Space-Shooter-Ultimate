f8b717921bd5c483812f9cbb484e4f6f199ffbb7
Filipe Cardoso <up202006409@up.pt>
2022-01-01 08:47:44 +0000

    Added background to the game

--- src/main/java/com/group605/spaceshooterultimate/Space.java
+++ src/main/java/com/group605/spaceshooterultimate/Space.java
@@ -1,7 +1,7 @@
 package com.group605.spaceshooterultimate;
 
-import com.googlecode.lanterna.SGR;
 import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.input.KeyStroke;
@@ -20,6 +20,10 @@ public class Space {
     }
 
     public void draw(TextGraphics graphics) throws IOException {
+        //Draws Game Background
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#737373"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+
         //Draws Character
         player.draw(graphics);
     }
