53df0aad9cc2438652b1ac170a60360eaa8796e7
Filipe Cardoso <up202006409@up.pt>
2022-01-01 08:08:00 +0000

    Created Position Class in order to master characters movement

---
+++ src/main/java/com/group605/spaceshooterultimate/Position.java
@@ -0,0 +1,33 @@
+package com.group605.spaceshooterultimate;
+
+import java.util.Objects;
+
+public class Position {
+
+    int x,y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Position position = (Position) o;
+        return x == position.x && y == position.y;
+    }
+
+}
