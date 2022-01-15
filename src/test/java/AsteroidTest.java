d1d44f24a2c8c4708685549c6088c9e5895ef8e9
Miguel Nogueira <miguel.nogueira0530@gmail.com>
2022-01-15 13:03:26 +0000

    Added asteroid test

---
+++ src/test/java/AsteroidTest.java
@@ -0,0 +1,31 @@
+import com.group605.spaceshooterultimate.Asteroid;
+import com.group605.spaceshooterultimate.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+
+
+public class AsteroidTest {
+    private Asteroid asteroid;
+
+    @BeforeEach
+    void setUp(){
+        asteroid = new Asteroid(10,10,5,"small");
+    }
+
+    @Test
+    void moveEnemy(){
+        asteroid.moveEnemy();
+        Assertions.assertEquals(11,asteroid.getPosition().getY());
+    }
+
+    @Test
+    void CheckImpact(){
+        Player player = Mockito.mock(Player.class);
+        Mockito.when(player.getPosition()).thenReturn(asteroid.getPosition());
+
+        Assertions.assertEquals(true,asteroid.checkImpact(asteroid,player));
+    }
+}
